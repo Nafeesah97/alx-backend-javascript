fecbe6f49554938e633a9b76783659436b92a511
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-03-09 00:16:43 +0000

    iterator

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,8 +1,5 @@
 export default function createEmployeesObject(departmentName, employees) {
-  const newArr = [];
-  for (const x of employees) {
-    const quote = "'";
-    newArr.push(quote + x + quote);
-  }
-  return `{ ${departmentName}: [ ${newArr} ] }`;
+  return {
+    [departmentName]: employees,
+  };
 }
