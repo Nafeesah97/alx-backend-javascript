b6f5f1f2702a39d70d3cd7d04c07d4e071c2886f
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-05-03 22:26:22 +0000

    calcul test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,15 +3,15 @@ const calculateNumber = require('./0-calcul.js');
 
 describe('calculateNumber', function() {
   it('checks the addition function', function() {
-    assert.equal(calculateNumber(2.5, 3.5), 7);
+    assert.strictequal(calculateNumber(2.5, 3.5), 7);
   });
   it('checks wthout rounding up', function() {
-    assert.equal(calculateNumber(2, 3), 5);
+    assert.strictequal(calculateNumber(2, 3), 5);
   });
   it('with second number being a float', function() {
-    assert.equal(calculateNumber(2, 3.5), 6);
+    assert.strictequal(calculateNumber(2, 3.5), 6);
   });
   it('first number is a  float', function() {
-    assert.equal(calculateNumber(2.5, 3), 6);
+    assert.strictequal(calculateNumber(2.5, 3), 6);
   });
 });
