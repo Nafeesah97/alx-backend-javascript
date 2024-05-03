89ee3adb227acee38e1d6779d55a76723f96119b
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-05-03 21:53:57 +0000

    calcul test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -5,5 +5,7 @@ describe('calculateNumber', function() {
   it('checks the addition function', function() {
     assert.equal(calculateNumber(2.5, 3.5), 7);
     assert.equal(calculateNumber(2.5, 3), 6);
+    assert.equal(calculateNumber(2, 3.5), 6);
+    assert.equal(calculateNumber(2, 3), 5);
   });
 });
