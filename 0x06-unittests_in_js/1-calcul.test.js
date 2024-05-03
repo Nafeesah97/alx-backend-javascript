ac67d10c35a04200140f114ac8357854c4051850
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-05-03 23:22:12 +0000

    more desc

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -6,6 +6,13 @@ describe('calculateNumber', () => {
     it('checks sum function', () => {
       assert.strictEqual(calculateNumber('SUM', 2.3, 1.8), 4);
     });
+    it('positive and negative numbers', () => {
+      assert.strictEqual(calculateNumber('SUM', 2.0, -2.0), 0);
+    });
+
+    it('0 and 0', () => {
+      assert.strictEqual(calculateNumber('SUM', 0.0, 0.0), 0);
+    });
   });
   describe('type == "SUBTRACT"', () => {
     it('checks subtract function', () => {
