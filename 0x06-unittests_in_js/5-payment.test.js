6258b0d366aa95c0b7717963f942bfa00a936e8a
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-05-04 20:16:41 +0000

    fixed

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -1,6 +1,6 @@
 const sinon = require('sinon');
 const { expect } = require('chai');
-const sendPaymentRequestToApi = require('./6-payment');
+const sendPaymentRequestToApi = require('./5-payment');
 
 describe('using hooks to check sendpaymenttoapi', () => {
   let pymentStub;
