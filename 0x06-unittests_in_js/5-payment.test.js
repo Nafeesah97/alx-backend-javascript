3bb0418e675ed946222be212e5e784b59254ee62
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-05-04 18:02:16 +0000

    hook using

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,24 @@
+const sinon = require('sinon');
+const Utils = require('./utils');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./6-payment');
+
+describe('using hooks to check sendpaymenttoapi', () => {
+  let pymentStub;
+  beforeEach(() => {
+    pymentstub = sinon.spy(console, 'log');
+  });
+  afterEach(() => {
+    pymentstub.restore();
+  });
+  it('checks the payment function', () => {
+    sendPaymentRequestToApi(100, 20);
+    expect(pymentstub.calledWith('The total is: 120')).to.be.true;
+    expect(pymentstub.callCount).to.be.equal(1);
+  });
+  it('checks the payment function', () => {
+    sendPaymentRequestToApi(10, 10);
+    expect(pymentstub.calledWith('The total is: 20')).to.be.true;
+    expect(pymentstub.callCount).to.be.equal(1);
+  });
+})
